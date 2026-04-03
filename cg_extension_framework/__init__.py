e09cc327d01546f5a42b0692a4fff8616916e28c
jcrichlake <jeff.crichlake@agile6.com>
2026-04-03 15:59:27 -0400

    Removing stale comment from init.py

--- cg_extension_framework/__init__.py
+++ cg_extension_framework/__init__.py
@@ -1,5 +1,3 @@
-# This file is auto-generated. Do not edit it manually — it will be overwritten
-# the next time `python -m common_grants_sdk.extensions.generate` is run.
 from __future__ import annotations
 
 from common_grants_sdk.extensions import Plugin
